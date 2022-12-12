bf72bd1de4b557930cd8151b8c3692bfb651d599
pswbuild <pswbuild@oppo.com>
2022-12-12 16:23:47 +0800

    Synchronize codes for OnePlus 10T 5G CPH2413/CPH2415_11_C.11 and OnePlus Ace Pro PGP110_11_C.11

---
+++ vendor/qcom/opensource/camera-kernel/drivers/oplus/cam_sensor_module/cam_actuator/oplus_cam_actuator_core.c
@@ -0,0 +1,462 @@
+#include <linux/module.h>
+#include "cam_sensor_cmn_header.h"
+#include "cam_actuator_core.h"
+#include "cam_sensor_util.h"
+#include "cam_trace.h"
+#include "cam_common_util.h"
+#include "cam_packet_util.h"
+
+#include "oplus_cam_actuator_core.h"
+
+#define ACTUATOR_REGISTER_SIZE 100
+uint32_t DW9817P[ACTUATOR_REGISTER_SIZE][2] = {
+	{0x02, 0x01},
+	{0x02, 0x00},
+	{0x02, 0x02},
+	{0x02, 0x00},
+	{0x06, 0x40},
+	{0x07, 0x39},
+	{0xff, 0xff},
+};
+
+uint32_t DW9817P_EEPROM[ACTUATOR_REGISTER_SIZE][2] = {
+	{0x0092, 0x00},
+	{0x0093, 0x00},
+	{0x0094, 0x00},
+	{0x0095, 0x00},
+	{0x0096, 0x00},
+	{0x0097, 0x00},
+	{0xffff, 0xff},
+};
+
+void cam_actuator_poll_setting_update(struct cam_actuator_ctrl_t *a_ctrl) {
+
+        struct i2c_settings_list *i2c_list = NULL;
+
+        a_ctrl->is_actuator_ready = true;
+        memset(&(a_ctrl->poll_register), 0, sizeof(struct cam_sensor_i2c_reg_array));
+        list_for_each_entry(i2c_list,
+                &(a_ctrl->i2c_data.init_settings.list_head), list) {
+                if (i2c_list->op_code == CAM_SENSOR_I2C_POLL) {
+                        a_ctrl->poll_register.reg_addr = i2c_list->i2c_settings.reg_setting[0].reg_addr;
+                        a_ctrl->poll_register.reg_data = i2c_list->i2c_settings.reg_setting[0].reg_data;
+                        a_ctrl->poll_register.data_mask = i2c_list->i2c_settings.reg_setting[0].data_mask;
+                        a_ctrl->poll_register.delay = 100; //i2c_list->i2c_settings.reg_setting[0].delay; // The max delay should be 100
+                        a_ctrl->addr_type = i2c_list->i2c_settings.addr_type;
+                        a_ctrl->data_type = i2c_list->i2c_settings.data_type;
+                }
+        }
+}
+
+void cam_actuator_poll_setting_apply(struct cam_actuator_ctrl_t *a_ctrl) {
+        int ret = 0;
+        if (!a_ctrl->is_actuator_ready) {
+                if (a_ctrl->poll_register.reg_addr || a_ctrl->poll_register.reg_data) {
+                        ret = camera_io_dev_poll(
+                                &(a_ctrl->io_master_info),
+                                a_ctrl->poll_register.reg_addr,
+                                a_ctrl->poll_register.reg_data,
+                                a_ctrl->poll_register.data_mask,
+                                a_ctrl->addr_type,
+                                a_ctrl->data_type,
+                                a_ctrl->poll_register.delay);
+                        if (ret < 0) {
+                                CAM_ERR(CAM_ACTUATOR,"i2c poll apply setting Fail: %d, is_actuator_ready %d", ret, a_ctrl->is_actuator_ready);
+                        } else {
+                                CAM_DBG(CAM_ACTUATOR,"is_actuator_ready %d, ret %d", a_ctrl->is_actuator_ready, ret);
+                        }
+                        a_ctrl->is_actuator_ready = true; //Just poll one time
+                }
+        }
+}
+
+int32_t cam_actuator_ramwrite(struct cam_actuator_ctrl_t *a_ctrl,
+	uint32_t addr, uint32_t data, unsigned short mdelay,enum camera_sensor_i2c_type addr_type,enum camera_sensor_i2c_type data_type)
+{
+	int32_t rc = 0;
+	int retry = 3;
+	int i = 0;
+	struct cam_sensor_i2c_reg_array i2c_write_setting = {
+		.reg_addr = addr,
+		.reg_data = data,
+		.delay = mdelay,
+		.data_mask = 0x00,
+	};
+	struct cam_sensor_i2c_reg_setting i2c_write = {
+		.reg_setting = &i2c_write_setting,
+		.size = 1,
+		.addr_type = addr_type,
+		.data_type = data_type,
+		.delay = 0,
+	};
+	if (a_ctrl == NULL) {
+		CAM_ERR(CAM_ACTUATOR, "Invalid Args");
+		return -EINVAL;
+	}
+
+	for(i = 0; i < retry; i++)
+	{
+		rc = camera_io_dev_write(&(a_ctrl->io_master_info), &i2c_write);
+		if (rc < 0) {
+			CAM_ERR(CAM_ACTUATOR, "write 0x%x failed, retry:%d device=%d master=%d", addr, i+1,a_ctrl->io_master_info.cci_client->cci_device,a_ctrl->io_master_info.cci_client->cci_i2c_master);
+		} else {
+			CAM_INFO(CAM_ACTUATOR, " write data addr:%x data:%x retry=%d",addr, data,i);
+			break;
+		}
+	}
+
+	return rc;
+}
+
+int32_t cam_actuator_construct_default_power_setting_oem(
+	struct cam_sensor_power_ctrl_t *power_info)
+{
+	int rc = 0;
+
+	power_info->power_setting_size = 2;
+	power_info->power_setting =
+		kzalloc(sizeof(struct cam_sensor_power_setting)*2,
+			GFP_KERNEL);
+	if (!power_info->power_setting)
+		return -ENOMEM;
+
+	power_info->power_setting[0].seq_type = SENSOR_VIO;
+	power_info->power_setting[0].seq_val = CAM_VIO;
+	power_info->power_setting[0].config_val = 1;
+	power_info->power_setting[0].delay = 1;
+
+	power_info->power_setting[1].seq_type = SENSOR_VAF;
+	power_info->power_setting[1].seq_val = CAM_VAF;
+	power_info->power_setting[1].config_val = 1;
+	power_info->power_setting[1].delay = 10;
+
+	power_info->power_down_setting_size = 2;
+	power_info->power_down_setting =
+		kzalloc(sizeof(struct cam_sensor_power_setting)*2,
+			GFP_KERNEL);
+	if (!power_info->power_down_setting) {
+		rc = -ENOMEM;
+		goto free_power_settings;
+	}
+
+	power_info->power_down_setting[0].seq_type = SENSOR_VAF;
+	power_info->power_down_setting[0].seq_val = CAM_VAF;
+	power_info->power_down_setting[0].config_val = 0;
+
+	power_info->power_down_setting[1].seq_type = SENSOR_VIO;
+	power_info->power_down_setting[1].seq_val = CAM_VIO;
+	power_info->power_down_setting[1].config_val = 0;
+
+	return rc;
+
+free_power_settings:
+	kfree(power_info->power_setting);
+	power_info->power_setting = NULL;
+	power_info->power_setting_size = 0;
+	return rc;
+}
+
+
+int cam_actuator_slaveInfo_pkt_parser_oem(struct cam_actuator_ctrl_t *a_ctrl)
+{
+	struct device_node     *of_node = NULL;
+	int                    ret = 0;
+	const char             *p = NULL;
+	struct cam_hw_soc_info *soc_info = &a_ctrl->soc_info;
+	of_node = soc_info->dev->of_node;
+
+	ret = of_property_read_u32(of_node, "is_aon_af", &a_ctrl->is_aon_af);
+	if (ret)
+	{
+		a_ctrl->is_aon_af = 0;
+		CAM_ERR(CAM_ACTUATOR, "get failed for is_aon_af = %d",a_ctrl->is_aon_af);
+	}
+	else
+	{
+		CAM_INFO(CAM_ACTUATOR, "read is_aon_af success, value:%d", a_ctrl->is_aon_af);
+	}
+
+	if(a_ctrl->is_aon_af == 0)
+	{
+		CAM_INFO(CAM_ACTUATOR, "not support aon af");
+		return 0;
+	}
+
+	ret = of_property_read_u32(of_node, "aon_af_slave", &a_ctrl->aon_af_slave);
+	if (ret)
+	{
+		a_ctrl->aon_af_slave = 0;
+		CAM_ERR(CAM_ACTUATOR, "get failed for aon_af_slave = 0x%x",a_ctrl->aon_af_slave);
+	}
+	else
+	{
+		CAM_INFO(CAM_ACTUATOR, "read aon_af_slave success, value:0x%x", a_ctrl->aon_af_slave);
+		a_ctrl->io_master_info.cci_client->sid = (a_ctrl->aon_af_slave >> 1);
+	}
+
+	ret = of_property_read_u32(of_node, "aon_eeprom_slave", &a_ctrl->aon_eeprom_slave);
+	if (ret)
+	{
+		a_ctrl->aon_eeprom_slave = 0;
+		CAM_ERR(CAM_ACTUATOR, "get failed for aon_eeprom_slave = 0x%x",a_ctrl->aon_eeprom_slave);
+	}
+	else
+	{
+		CAM_INFO(CAM_ACTUATOR, "read aon_eeprom_slave success, value:0x%x", a_ctrl->aon_eeprom_slave);
+		a_ctrl->io_master_info.cci_client->sid = (a_ctrl->aon_af_slave >> 1);
+	}
+
+	ret = of_property_read_string_index(of_node, "actuator,name", 0, (const char **)&p);
+	if (ret)
+	{
+		memcpy(a_ctrl->aon_af_name, "aon_af_name", sizeof(a_ctrl->aon_af_name));
+		CAM_ERR(CAM_ACTUATOR, "get actuator,name failed rc:%d, set default value to %s", ret, a_ctrl->aon_af_name);
+	}
+	else
+	{
+		memcpy(a_ctrl->aon_af_name, p, sizeof(a_ctrl->aon_af_name));
+		CAM_INFO(CAM_ACTUATOR, "read actuator,name success, value:%s", a_ctrl->aon_af_name);
+	}
+
+	a_ctrl->io_master_info.cci_client->i2c_freq_mode = I2C_FAST_PLUS_MODE;
+	a_ctrl->io_master_info.cci_client->retries = 3;
+	a_ctrl->io_master_info.cci_client->id_map = 0;
+	a_ctrl->aon_af_power = CAM_ACTUATOR_POWER_OFF;
+	a_ctrl->io_master_info.cci_client->cci_i2c_master = a_ctrl->cci_i2c_master;
+
+	return 0;
+}
+
+int cam_actuator_set_initsetting(struct cam_actuator_ctrl_t *a_ctrl)
+{
+	int ret = 0;
+	int i = 0;
+
+	if(strstr(a_ctrl->aon_af_name, "dw9718p"))
+	{
+		for (i = 0; i < ACTUATOR_REGISTER_SIZE; i++)
+		{
+			if ( (DW9817P[i][0] != 0xff) && (DW9817P[i][1] != 0xff) )
+			{
+				ret = cam_actuator_ramwrite(a_ctrl,
+					(uint32_t)DW9817P[i][0],
+					(uint32_t)DW9817P[i][1],
+					1,
+					CAMERA_SENSOR_I2C_TYPE_BYTE,
+					CAMERA_SENSOR_I2C_TYPE_BYTE);
+				if(ret < 0)
+				{
+					CAM_ERR(CAM_ACTUATOR,"write failed ret : %d", ret);
+					return ret;
+				}
+			}
+			else
+			{
+				CAM_INFO(CAM_ACTUATOR,"set initting setting success ");
+				break;
+			}
+		}
+	}
+	else
+	{
+		CAM_ERR(CAM_ACTUATOR,"not support this aon_af ");
+	}
+
+	return ret;
+}
+
+int cam_actuator_get_infinity_postion(struct cam_actuator_ctrl_t *a_ctrl)
+{
+	uint32_t a_sid = 0;
+	int i = 0;
+	int ret = 0;
+	a_sid = a_ctrl->io_master_info.cci_client->sid;
+	a_ctrl->io_master_info.cci_client->sid = (a_ctrl->aon_eeprom_slave >> 1);
+
+	if(strstr(a_ctrl->aon_af_name, "dw9718p"))
+	{
+		for (i = 0; i < ACTUATOR_REGISTER_SIZE; i++)
+		{
+			if (DW9817P_EEPROM[i][0] != 0xffff)
+			{
+				ret = camera_io_dev_read(&(a_ctrl->io_master_info),
+					(uint32_t)DW9817P_EEPROM[i][0],
+					(uint32_t *)&DW9817P_EEPROM[i][1],
+					CAMERA_SENSOR_I2C_TYPE_WORD,
+					CAMERA_SENSOR_I2C_TYPE_BYTE);
+				if(ret < 0)
+				{
+					CAM_ERR(CAM_ACTUATOR,"read failed ret : %d addr = 0x%x", ret, DW9817P_EEPROM[i][0]);
+				}
+				else
+				{
+					CAM_INFO(CAM_ACTUATOR,"read success addr = 0x%x data=0x%x", DW9817P_EEPROM[i][0],DW9817P_EEPROM[i][1]);
+				}
+			}
+			else
+			{
+				CAM_ERR(CAM_ACTUATOR,"get infinity_postion success ");
+				break;
+			}
+		}
+		a_ctrl->macroPos = DW9817P_EEPROM[1][1] << 8 | DW9817P_EEPROM[0][1];
+		a_ctrl->infPos   = DW9817P_EEPROM[3][1] << 8 | DW9817P_EEPROM[2][1];
+		a_ctrl->middPos  = DW9817P_EEPROM[5][1] << 8 | DW9817P_EEPROM[4][1];
+		CAM_INFO(CAM_ACTUATOR,"a_ctrl->macroPos = %d ; infPos =%d ; middPos=%d",a_ctrl->macroPos,a_ctrl->infPos,a_ctrl->middPos);
+
+	}
+	else
+	{
+		CAM_ERR(CAM_ACTUATOR,"not support this aon_af ");
+	}
+
+	a_ctrl->io_master_info.cci_client->sid = a_sid;
+	return ret;
+}
+
+int cam_actuator_enble(struct cam_actuator_ctrl_t *a_ctrl)
+{
+	int rc = 0;
+
+	if(a_ctrl->is_aon_af != 0)
+	{
+		if(a_ctrl->aon_af_power == CAM_ACTUATOR_POWER_OFF)
+		{
+			rc = cam_actuator_power_up(a_ctrl);
+			if(rc)
+			{
+				CAM_ERR(CAM_ACTUATOR,"cam_actuator_power up failed");
+				mutex_unlock(&(a_ctrl->actuator_mutex));
+				return rc;
+			}
+			else
+			{
+				CAM_INFO(CAM_ACTUATOR,"cam_actuator_power up success");
+				a_ctrl->aon_af_power = CAM_ACTUATOR_POWER_ON;
+			}
+		}
+
+		rc = cam_actuator_set_initsetting(a_ctrl);
+		if(rc)
+		{
+			CAM_ERR(CAM_ACTUATOR,"cam_actuator_set_initsetting up failed");
+			mutex_unlock(&(a_ctrl->actuator_mutex));
+			return rc;
+		}
+		else
+		{
+			CAM_INFO(CAM_ACTUATOR,"cam_actuator_set_initsetting  success");
+		}
+		CAM_ERR(CAM_ACTUATOR,"cam_actuator_enble success");
+
+		rc = cam_actuator_get_infinity_postion(a_ctrl);
+		if(rc)
+		{
+			CAM_ERR(CAM_ACTUATOR,"cam_actuator_get_infinity_postion failed ret : %d", rc);
+		}
+
+	}
+	else
+	{
+		CAM_ERR(CAM_ACTUATOR,"this sensor not surport aon af");
+	}
+
+	return rc;
+}
+
+int cam_actuator_setfocus(struct cam_actuator_ctrl_t *a_ctrl)
+{
+	int ret = 0;
+
+	if(a_ctrl->is_aon_af != 0)
+	{
+		if(strstr(a_ctrl->aon_af_name, "dw9718p"))
+		{
+			ret = cam_actuator_ramwrite(a_ctrl,
+					0x03,
+					a_ctrl->middPos,
+					1,
+					CAMERA_SENSOR_I2C_TYPE_BYTE,
+					CAMERA_SENSOR_I2C_TYPE_WORD);
+			if(ret < 0)
+			{
+				CAM_ERR(CAM_ACTUATOR,"write failed ret : %d", ret);
+				return ret;
+			}
+		}
+	}
+	return ret;
+}
+
+int cam_actuator_disable(struct cam_actuator_ctrl_t *a_ctrl)
+{
+	int rc = 0;
+	CAM_INFO(CAM_ACTUATOR,"a_ctrl->is_aon_af=%d a_ctrl->aon_af_power=%d",a_ctrl->is_aon_af,a_ctrl->aon_af_power);
+	if(a_ctrl->is_aon_af != 0 && a_ctrl->aon_af_power==CAM_ACTUATOR_POWER_ON)
+	{
+		rc = cam_actuator_power_down(a_ctrl);
+		if(rc)
+		{
+			CAM_ERR(CAM_ACTUATOR,"cam_actuator_power_down failed");
+			mutex_unlock(&(a_ctrl->actuator_mutex));
+			return rc;
+		}
+		else
+		{
+			CAM_ERR(CAM_ACTUATOR,"cam_actuator_power_down success");
+			a_ctrl->aon_af_power = CAM_ACTUATOR_POWER_OFF;
+		}
+	}
+	else
+	{
+		CAM_ERR(CAM_ACTUATOR,"this sensor not surport aon af");
+	}
+	CAM_ERR(CAM_ACTUATOR,"cam_actuator_disable success");
+	return rc;
+}
+
+int32_t oplus_cam_actuator_driver_cmd(struct cam_actuator_ctrl_t *a_ctrl, void *arg)
+{
+	int rc = 0;
+	struct cam_control *cmd = (struct cam_control *)arg;
+	switch (cmd->op_code) {
+		case CAM_OEM_ACTUATOR_SETFOCUS: {
+			rc = cam_actuator_setfocus(a_ctrl);
+			if(rc)
+			{
+				CAM_ERR(CAM_ACTUATOR,"cam_actuator_enble failed");
+			}
+			break;
+		}
+		case CAM_OEM_ACTUATOR_GET_ID: {
+			rc = cam_actuator_slaveInfo_pkt_parser_oem(a_ctrl);
+			if(rc)
+			{
+				CAM_ERR(CAM_ACTUATOR,"cam_actuator_get_infinity_postion failed ret : %d", rc);
+			}
+			if (copy_to_user((void __user *)cmd->handle,&a_ctrl->is_aon_af,sizeof(uint32_t)))
+			{
+				CAM_ERR(CAM_ACTUATOR,"copy camera id to user fail ");
+			}
+			break;
+		}
+		case CAM_OEM_ACTUATOR_STATR: {
+			rc = cam_actuator_enble(a_ctrl);
+			if(rc)
+			{
+				CAM_ERR(CAM_ACTUATOR,"cam_actuator_enble failed");
+			}
+			break;
+		}
+		case CAM_OEM_ACTUATOR_STOP: {
+			rc = cam_actuator_disable(a_ctrl);
+			if(rc)
+			{
+				CAM_ERR(CAM_ACTUATOR,"cam_actuator_disable failed");
+			}
+			break;
+		}
+	}
+	return rc;
+}
+
---
+++ vendor/qcom/opensource/display-drivers/msm/dsi/iris/dsi_iris_loop_back.c
@@ -0,0 +1,421 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (c) 2015-2019, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2017-2020, Pixelworks, Inc.
+ *
+ * These files contain modifications made by Pixelworks, Inc., in 2019-2020.
+ */
+#include <linux/kobject.h>
+#include "dsi_iris_api.h"
+#include "dsi_iris_i3c.h"
+#include "dsi_iris_loop_back.h"
+#include "dsi_iris_lightup.h"
+#include "dsi_iris_lightup_ocp.h"
+#include "dsi_iris_log.h"
+#include "dsi_iris_lp.h"
+#include "sde_connector.h"
+
+static void iris_set_esd_status(bool enable)
+{
+	struct iris_cfg *pcfg = iris_get_cfg();
+	struct dsi_display *display = pcfg->display;
+
+	if (!display) {
+		return;
+	}
+
+	if (!pcfg->panel || !display->drm_conn) {
+		return;
+	}
+
+	if (!enable) {
+		if (display->panel->esd_config.esd_enabled) {
+			sde_connector_schedule_status_work(display->drm_conn, false);
+			display->panel->esd_config.esd_enabled = false;
+			IRIS_LOGD("disable esd work");
+		}
+	} else {
+		if (!display->panel->esd_config.esd_enabled) {
+			sde_connector_schedule_status_work(display->drm_conn, true);
+			display->panel->esd_config.esd_enabled = true;
+			IRIS_LOGD("enabled esd work");
+		}
+	}
+}
+
+static uint32_t iris_loop_back_flag = 0xffffffff;
+
+/**
+ * pure loop back
+ */
+#define PURE_LOOP_BACK_REG_NUM  (51)
+static uint32_t pure_loop_back_op_addr[PURE_LOOP_BACK_REG_NUM] = {
+0xf00000f8, 0xf0000068, 0xf0000068, 0xf0000068, 0xf0000068,
+0xf1480068, 0xf1480074, 0xf1480078, 0xf148007c, 0xf1480080,
+0xf14800b8, 0xf14800b4, 0xf1480120, 0xf1a8001c, 0xf1a80020,
+0xf1a80024, 0xf1a80028, 0xf1a80060, 0xf1a8005c, 0xf1a9ff00,
+0xf1ac0000, 0xf1ac0010, 0xf1ac001c, 0xf1ac0020, 0xf1ac0024,
+0xf1ac0028, 0xf1ac0060, 0xf1ac005c, 0xf1adff00, 0xf1980000,
+0xf1980008, 0xf1980048, 0xf198007c, 0xf1980050, 0xf1980054,
+0xf1981010, 0xf1940000, 0xf1940018, 0xf194006c, 0xf1940008,
+0xf19401c4, 0xf194106c, 0xf18c0004, 0xf18d1000, 0xf178001c,
+0xf1780128, 0xf1780124, 0xf179ff00, 0xf1700130, 0xf1711000,
+0xf1941068
+};
+
+static uint32_t pure_loop_back_op_val[PURE_LOOP_BACK_REG_NUM] = {
+0x00000644, 0x00000000, 0x00000001, 0x00000003, 0x000000ff,
+0x80078030, 0x0e025901, 0xd1002000, 0x0c000700, 0x5c066706,
+0x346cf43b, 0xb42b342b, 0x00000002, 0x0e025901, 0xd1002000,
+0x0c000700, 0x5c066706, 0x346cf43b, 0xb42b342b, 0x00000100,
+0x01480150, 0x80078030, 0x0e025901, 0xd1002000, 0x0c000700,
+0x5c066706, 0x346cf43b, 0xb42b342b, 0x00000100, 0x00000a04,
+0x00001800, 0x0438021c, 0x00000003, 0x00040000, 0x00040000,
+0x00000010, 0x0001400c, 0x0438021c, 0x00000aaa, 0x84400050,
+0x00000003, 0x00000200, 0x00000008, 0x00000100, 0x30080800,
+0x01040000, 0x10100102, 0x00000100, 0x0a0a0664, 0x00000006,
+0x00000037
+};
+
+static uint32_t pure_loop_back_op_delay[PURE_LOOP_BACK_REG_NUM] = {
+0, 1, 1, 1, 1,
+0, 0, 0, 0, 0,
+0, 0, 0, 0, 0,
+0, 0, 0, 0, 0,
+0, 0, 0, 0, 0,
+0, 0, 0, 0, 0,
+0, 0, 0, 0, 0,
+0, 0, 0, 0, 0,
+0, 0, 0, 0, 0,
+0, 0, 0, 0, 0,
+0
+};
+
+static uint32_t pure_loop_back_checksum[3] = {0x58f75b40, 0x57ee3d0d, 0x576d2193};
+
+static int iris_pure_loop_back_verify(void)
+{
+	struct iris_cfg *pcfg = iris_get_cfg();
+	int ret = 0;
+	int i = 0;
+	uint32_t statis[3] = {0};
+
+	IRIS_LOGD("%s(%d), start.", __func__, __LINE__);
+	iris_set_esd_status(false);
+	iris_reset();
+	mdelay(100);
+	mutex_lock(&pcfg->panel->panel_lock);
+	iris_set_two_wire0_enable();
+	mutex_unlock(&pcfg->panel->panel_lock);
+
+	for (i = 0; i < PURE_LOOP_BACK_REG_NUM; i++) {
+		ret = iris_ioctl_i2c_write(pure_loop_back_op_addr[i], pure_loop_back_op_val[i]);
+		if (ret) {
+			IRIS_LOGE("%s(%d), error, i = %d, ret = %d.", __func__, __LINE__, i, ret);
+			return 0x11;
+		}
+
+		if (pure_loop_back_op_delay[i])
+			mdelay(pure_loop_back_op_delay[i]);
+	}
+
+	mdelay(100);
+	for (i = 0; i < 3; i++) {
+		iris_ioctl_i2c_read(0xf1940200 + i*4, &statis[i]);
+		if (ret) {
+			IRIS_LOGE("%s(%d), i2c read statis fail.", __func__, __LINE__);
+			return 0x12;
+		}
+	}
+	iris_set_esd_status(true);
+	IRIS_LOGI("%s(%d), statis = 0x%x 0x%x 0x%x.", __func__, __LINE__,
+			statis[0], statis[1], statis[2]);
+
+
+	if ((statis[0] == pure_loop_back_checksum[0]) &&
+		(statis[1] == pure_loop_back_checksum[1]) &&
+		(statis[2] == pure_loop_back_checksum[2])) {
+		ret = ERR_NO_ERR;
+		IRIS_LOGI("%s(%d), loopback validate success.", __func__, __LINE__);
+	} else {
+		IRIS_LOGE("%s(%d), statis not equal to checksum.", __func__, __LINE__);
+		return 0x13;
+	}
+
+	IRIS_LOGD("%s(%d), end.", __func__, __LINE__);
+
+	return ret;
+}
+
+/**
+ * dual pt
+ */
+#define DUAL_PT_REG_NUM  (47)
+static uint32_t dual_pt_op_addr[DUAL_PT_REG_NUM] = {
+0xf0000068, 0xf0000068, 0xf0000000, 0xf00000f8, 0xf1480068,
+0xf1480074, 0xf1480078, 0xf148007c, 0xf1480080, 0xf14800b8,
+0xf14800b4, 0xf1a8001c, 0xf1a80020, 0xf1a80024, 0xf1a80028,
+0xf1a80060, 0xf1a8005c, 0xf1ac001c, 0xf1ac0020, 0xf1ac0024,
+0xf1ac0028, 0xf1ac0000, 0xf1ac0010, 0xf1ac0060, 0xf1ac005c,
+0xf1980000, 0xf1980048, 0xf1980050, 0xf1980054, 0xf1980008,
+0xf18c0004, 0xf18d1000, 0xf178001c, 0xf1780124, 0xf1780128,
+0xf179ff00, 0xf1700130, 0xf1711000, 0xf198007c, 0xf1981010,
+0xf1940000, 0xf1940018, 0xf194006c, 0xf1940008, 0xf19401c4,
+0xf194106c, 0xf1941068
+};
+
+static uint32_t dual_pt_op_val[DUAL_PT_REG_NUM] = {
+0x00000001, 0x000000ff, 0x00000000, 0x00000644, 0x80078030,
+0x0e025901, 0xd1002000, 0x0c000700, 0x5c066706, 0x346cf43b,
+0xb42b342b, 0x0e025901, 0xd1002000, 0x0c000700, 0x5c066706,
+0x346cf43b, 0xb42b342b, 0x0e025901, 0xd1002000, 0x0c000700,
+0x5c066706, 0x01480150, 0x80078030, 0x346cf43b, 0xb42b342b,
+0x00000a04, 0x0438021c, 0x00040000, 0x00040000, 0x00001800,
+0x00000008, 0x00000100, 0x30080800, 0x10100102, 0x01040000,
+0x00000100, 0x0a0a0664, 0x00000002, 0x00000003, 0x00000010,
+0x0001400c, 0x0438021c, 0x00000aaa, 0x84400050, 0x00000003,
+0x00000200, 0x00000037
+};
+
+static uint32_t dual_pt_op_delay[DUAL_PT_REG_NUM] = {
+0, 1, 0, 1, 0,
+0, 0, 0, 0, 0,
+0, 0, 0, 0, 0,
+0, 0, 0, 0, 0,
+0, 0, 0, 0, 0,
+0, 0, 0, 0, 0,
+0, 0, 0, 0, 0,
+0, 0, 0, 0, 0,
+0, 0, 0, 0, 0,
+0, 0,
+};
+
+static uint32_t dual_pt_checksum[3] = {0x58f75b40, 0x57ee3d0d, 0x576d2193};
+
+static int iris_dual_pt_verify(void)
+{
+	struct iris_cfg *pcfg = iris_get_cfg();
+	int ret = 0;
+	int i = 0;
+	uint32_t statis[3] = {0};
+
+	IRIS_LOGD("%s(%d), start.", __func__, __LINE__);
+	iris_set_esd_status(false);
+	iris_reset();
+	mdelay(100);
+	mutex_lock(&pcfg->panel->panel_lock);
+	iris_set_two_wire0_enable();
+	mutex_unlock(&pcfg->panel->panel_lock);
+
+	for (i = 0; i < DUAL_PT_REG_NUM; i++) {
+		ret = iris_ioctl_i2c_write(dual_pt_op_addr[i], dual_pt_op_val[i]);
+		if (ret) {
+			IRIS_LOGE("%s(%d), error, i = %d, ret = %d.", __func__, __LINE__, i, ret);
+			return 0x21;
+		}
+
+		if (dual_pt_op_delay[i])
+			mdelay(dual_pt_op_delay[i]);
+	}
+
+	mdelay(100);
+	for (i = 0; i < 3; i++) {
+		iris_ioctl_i2c_read(0xf1940200 + i*4, &statis[i]);
+		if (ret) {
+			IRIS_LOGE("%s(%d), i2c read statis fail.", __func__, __LINE__);
+			return 0x22;
+		}
+	}
+	iris_set_esd_status(true);
+	IRIS_LOGI("%s(%d), statis = 0x%x 0x%x 0x%x.", __func__, __LINE__,
+			statis[0], statis[1], statis[2]);
+
+
+	if ((statis[0] == dual_pt_checksum[0]) &&
+		(statis[1] == dual_pt_checksum[1]) &&
+		(statis[2] == dual_pt_checksum[2])) {
+		ret = ERR_NO_ERR;
+		IRIS_LOGI("%s(%d), loopback validate success.", __func__, __LINE__);
+	} else {
+		IRIS_LOGE("%s(%d), statis not equal to checksum.", __func__, __LINE__);
+		return 0x23;
+	}
+
+	IRIS_LOGD("%s(%d), end.", __func__, __LINE__);
+
+	return ret;
+}
+
+int iris_loop_back_validate(void)
+{
+	int ret = 0;
+
+	if (iris_loop_back_flag & BIT_PURE_LOOPBACK) {
+		IRIS_LOGI("%s(%d), step 1, pure loop back verify!", __func__, __LINE__);
+		ret = iris_pure_loop_back_verify();
+		if (ret) {
+			IRIS_LOGE("%s(%d) step1: pure loop back verify ret = %d", __func__, __LINE__, ret);
+			return ERR_PURE_LOOP_BACK;
+		}
+	}
+
+	if (iris_loop_back_flag & BIT_DUAL_PT) {
+		IRIS_LOGI("%s(%d), step 2, dual pt verify!", __func__, __LINE__);
+		ret = iris_dual_pt_verify();
+		if (ret) {
+			IRIS_LOGE("%s(%d) step2: dual pt verify ret = %d", __func__, __LINE__, ret);
+			return ERR_DUAL_PT;
+		}
+	}
+	iris_reset();
+	mdelay(10);
+	_iris_bulksram_power_domain_proc();
+	_iris_disable_temp_sensor();
+	iris_sleep_abyp_power_down();
+	return 0;
+}
+
+static ssize_t _iris_dbg_loop_back_read(struct file *file,
+		char __user *buff, size_t count, loff_t *ppos)
+{
+	int ret = -1;
+	int tot = 0;
+	char bp[512];
+
+	if (*ppos)
+		return 0;
+
+	ret = iris_loop_back_validate();
+	IRIS_LOGI("%s(%d) ret: %d", __func__, __LINE__, ret);
+
+	tot = scnprintf(bp, sizeof(bp), "0x%x\n", ret);
+	if (copy_to_user(buff, bp, tot))
+		return -EFAULT;
+
+	*ppos += tot;
+
+	return tot;
+}
+
+static const struct file_operations iris_loop_back_fops = {
+	.open = simple_open,
+	.read = _iris_dbg_loop_back_read,
+};
+
+
+int iris_mipi_rx0_validate(void)
+{
+	int ret = 0;
+	uint32_t reg_val = 0;
+
+	iris_ocp_write_val(0xf1a00008, 0xffff3a9a);
+	mdelay(100);
+	reg_val = iris_ocp_read(0xf1a00004, DSI_CMD_SET_STATE_HS);
+	if (reg_val & 0x0ffff3cf)
+		ret = 1;
+	else
+		ret = 0;
+
+	return ret;
+}
+
+static ssize_t _iris_mipi_rx_validate(struct file *file, char __user *buff,
+		size_t count, loff_t *ppos)
+{
+	int ret = 0;
+	int tot = 0;
+	char bp[512];
+	struct iris_cfg *pcfg = iris_get_cfg();
+
+	if (*ppos)
+		return 0;
+
+	mutex_lock(&pcfg->panel->panel_lock);
+	ret = iris_mipi_rx0_validate();
+	mutex_unlock(&pcfg->panel->panel_lock);
+
+	tot = scnprintf(bp, sizeof(bp), "0x%02x\n", ret);
+	if (copy_to_user(buff, bp, tot))
+		return -EFAULT;
+	*ppos += tot;
+
+	return tot;
+
+}
+
+static const struct file_operations iris_mipi_rx_validate_fops = {
+	.open = simple_open,
+	.write = NULL,
+	.read = _iris_mipi_rx_validate,
+};
+
+static ssize_t iris_loop_back_show(struct kobject *obj, struct kobj_attribute *attr, char *buf)
+{
+	int ret;
+
+	ret = iris_loop_back_validate();
+	IRIS_LOGI("%s(%d) ret: %d", __func__, __LINE__, ret);
+
+	return snprintf(buf, PAGE_SIZE, "0x%x\n", ret);
+}
+
+#define IRIS_ATTR(_name, _mode, _show, _store) \
+struct kobj_attribute iris_attr_##_name = __ATTR(_name, _mode, _show, _store)
+static IRIS_ATTR(iris_loop_back, S_IRUGO, iris_loop_back_show, NULL);
+
+static struct attribute *iris_dev_attrs[] = {
+    &iris_attr_iris_loop_back.attr,
+    NULL
+};
+
+static const struct attribute_group iris_attr_group = {
+    .attrs = iris_dev_attrs,
+};
+
+int iris_dbgfs_loop_back_init(struct dsi_display *display)
+{
+	struct iris_cfg *pcfg = iris_get_cfg();
+	int retval;
+	if (pcfg->iris_kobj == NULL) {
+		pcfg->iris_kobj = kobject_create_and_add(IRIS_SYSFS_TOP_DIR, kernel_kobj);
+	} else {
+		if (pcfg->iris_kobj) {
+			/* Create the files associated with this kobject */
+			retval = sysfs_create_group(pcfg->iris_kobj, &iris_attr_group);
+			if (retval) {
+				kobject_put(pcfg->iris_kobj);
+				IRIS_LOGW("sysfs create group iris_dbg_display_mode_show error");
+			}
+		} else {
+			IRIS_LOGW("sysfs create group iris dir error");
+			return -ENODEV;
+		}
+	}
+	if (pcfg->dbg_root == NULL) {
+		pcfg->dbg_root = debugfs_create_dir("iris", NULL);
+		if (IS_ERR_OR_NULL(pcfg->dbg_root)) {
+			IRIS_LOGE("debugfs_create_dir for iris_debug failed, error %ld",
+					PTR_ERR(pcfg->dbg_root));
+			return -ENODEV;
+		}
+	}
+
+	debugfs_create_u32("iris_loop_back_flag", 0644, pcfg->dbg_root,
+			(u32 *)&iris_loop_back_flag);
+
+	if (debugfs_create_file("iris_loop_back",	0644, pcfg->dbg_root, display,
+				&iris_loop_back_fops) == NULL) {
+		IRIS_LOGE("%s(%d): debugfs_create_file: index fail",
+				__FILE__, __LINE__);
+		return -EFAULT;
+	}
+
+	if (debugfs_create_file("iris_mipi_rx_validate",	0644, pcfg->dbg_root, display,
+				&iris_mipi_rx_validate_fops) == NULL) {
+		IRIS_LOGE("%s(%d): debugfs_create_file: index fail",
+				__FILE__, __LINE__);
+		return -EFAULT;
+	}
+
+	return 0;
+}
